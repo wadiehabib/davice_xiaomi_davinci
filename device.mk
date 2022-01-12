bad2faa669fa8140d49ba0a7435e2beaab7c7315
wadiehabib <wadie4jesus@gmail.com>
2022-01-12 23:12:44 +0000

    davinci: dont ship gcam go

--- device.mk
+++ device.mk
@@ -133,8 +133,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:64 \
     android.hardware.camera.provider@2.4-service_64
 
-PRODUCT_PACKAGES += \
-    CameraGo
+#PRODUCT_PACKAGES += \
+#    CameraGo
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
