7991aa8837cc6dc33046f705966f55e7570a0f52
Wadie Habib <84630485+wadiehabib@users.noreply.github.com>
2022-01-28 03:22:11 +0200

    ancient: force using lawnchair on vanilla variant

--- ancient_davinci.mk
+++ ancient_davinci.mk
@@ -14,14 +14,15 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 # Inherit some common ancient stuff.
 $(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-IS_PHONE := true
-ANCIENT_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
+#IS_PHONE := true
+#ANCIENT_GAPPS := true
+#TARGET_GAPPS_ARCH := arm64
 ANCIENT_OFFICIAL := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-FORCE_OTA := true
+#FORCE_OTA := true
 PIXEL_STUFF := true
 EXTRA_UDFPS_ANIMATIONS := true
+FORCE_LAWNCHAIR=true
 
 PRODUCT_NAME := ancient_davinci
 PRODUCT_DEVICE := davinci
